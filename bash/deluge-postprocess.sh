2ffe70bf813f9e4c38339ffa1b647186e19ed9c4
Reinhard Pointner <reinhard.pointner@gmail.com>
2016-03-04 13:46:37 +0800

    sanity checks

--- bash/deluge-postprocess.sh
+++ bash/deluge-postprocess.sh
@@ -8,4 +8,10 @@ export OPT_LABEL="N/A"
 # Configuration
 export CONF_OUTPUT="$HOME/Media" # if this script is called by the deluge user, then $HOME will NOT refer to YOUR user home, but paths such as /var/lib/deluge instead
 
+# Sanity Check
+if [ ! -w "$HOME" ]; then
+	echo "$HOME must be writable" 1>&2 # filebot requires a valid $HOME folder
+	exit 2
+fi
+
 filebot -script fn:amc --output "$CONF_OUTPUT" --action duplicate --conflict skip -non-strict --log-file amc.log --def unsorted=y music=y artwork=y excludeList=".excludes" ut_dir="$OPT_PATH" ut_kind="multi" ut_title="$OPT_NAME" ut_label="$OPT_LABEL"
