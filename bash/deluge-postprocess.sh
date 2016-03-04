2c9d48fb96d9e0b11b4c99032407d9624e2ba8eb
Reinhard Pointner <reinhard.pointner@gmail.com>
2016-03-04 12:04:08 +0800

    Added n00b help

--- bash/deluge-postprocess.sh
+++ bash/deluge-postprocess.sh
@@ -6,6 +6,6 @@ export OPT_NAME="$2"
 export OPT_LABEL="N/A"
 
 # Configuration
-export CONF_OUTPUT="$HOME/Media"
+export CONF_OUTPUT="$HOME/Media" # if this script is called by the deluge user, then $HOME will NOT refer to YOUR user home, but paths such as /var/lib/deluge instead
 
 filebot -script fn:amc --output "$CONF_OUTPUT" --action duplicate --conflict skip -non-strict --log-file amc.log --def unsorted=y music=y artwork=y excludeList=".excludes" ut_dir="$OPT_PATH" ut_kind="multi" ut_title="$OPT_NAME" ut_label="$OPT_LABEL"
