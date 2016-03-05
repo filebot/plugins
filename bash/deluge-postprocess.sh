12112a1f5375a90206c98f27433b6ae481f2e34f
Reinhard Pointner <reinhard.pointner@gmail.com>
2016-03-05 23:47:55 +0800

    Sanity checks should be part of filebot.sh

--- bash/deluge-postprocess.sh
+++ bash/deluge-postprocess.sh
@@ -8,10 +8,4 @@ ARG_LABEL="N/A"
 # Configuration
 CONFIG_OUTPUT="$HOME/Media" # if this script is called by the deluge user, then $HOME will NOT refer to YOUR user home, but paths such as /var/lib/deluge instead
 
-# Sanity Check
-if [ ! -w "$HOME" ]; then
-	echo "$HOME must be writable" 1>&2 # filebot requires a valid $HOME folder
-	exit 2
-fi
-
 filebot -script fn:amc --output "$CONFIG_OUTPUT" --action duplicate --conflict skip -non-strict --log-file amc.log --def unsorted=y music=y artwork=y excludeList=".excludes" ut_dir="$ARG_PATH" ut_kind="multi" ut_title="$ARG_NAME" ut_label="$ARG_LABEL"
