f7a2de00ac2c8fc24f3bcabfc138b7086c238db0
Reinhard Pointner <reinhard.pointner@gmail.com>
2016-04-01 16:28:59 +0800

    Fix https://www.filebot.net/forums/viewtopic.php?f=4&t=3521&p=20014#p20014

--- js/JDownloader.ArchiveExtracted.js
+++ js/JDownloader.ArchiveExtracted.js
@@ -1,22 +1,19 @@
 var script = JD_HOME + '/jdownloader-postprocess.sh'
 
-var path = archive.getFolder()
-var name = archive.getName()
 var links = archive.getDownloadLinks() ? archive.getDownloadLinks() : []
 var package = links.length > 0 ? links[0].getPackage() : null
-var label = package && package.getComment() ? package.getComment() : 'N/A'
 
 function isReallyFinished() {
 	for (var i = 0; i < links.length; i++) {
-		if (links[i].getArchive() != null && links[i].getExtractionStatus() != "SUCCESSFUL" || package && !package.isFinished()) {
+		if (links[i].getArchive() != null && links[i].getExtractionStatus() != "SUCCESSFUL" || !package.isFinished()) {
 			return false
 		}
 	}
 	return true
 }
 
-if (isReallyFinished()) {
-	var command = [script, path, name, label, 'ARCHIVE_EXTRACTED']
+if (package && isReallyFinished()) {
+	var command = [script, package.getDownloadFolder(), package.getName(), package.getComment() ? package.getComment() : 'N/A', 'ARCHIVE_EXTRACTED']
 	log(command)
 	log(callSync(command))
 }
