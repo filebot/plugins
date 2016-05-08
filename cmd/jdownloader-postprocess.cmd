1e17d553764d57f541eaf8d17af924f252c0c5c0
Reinhard Pointner <reinhard.pointner@gmail.com>
2016-05-08 17:20:28 +0800

    + jdownloader-postprocess.cmd

--- cmd/jdownloader-postprocess.cmd
+++ cmd/jdownloader-postprocess.cmd
@@ -8,4 +8,4 @@ set ARG_LABEL=%3
 :: Configuration
 set CONFIG_OUTPUT=%USERPROFILE%/Media
 
-filebot -script fn:amc --output "%CONFIG_OUTPUT%" --action duplicate --conflict skip -non-strict --log-file amc.log --def skipExtract=y unsorted=y music=y artwork=y excludeList=".excludes" ut_dir="%ARG_PATH%" ut_kind="multi" ut_title="%ARG_NAME%" ut_label="%ARG_LABEL%"
+filebot -script fn:amc --output %CONFIG_OUTPUT% --action duplicate --conflict skip -non-strict --log-file amc.log --def skipExtract=y unsorted=y music=y artwork=y excludeList=excludes.txt ut_dir=%ARG_PATH% ut_kind=multi ut_title=%ARG_NAME% ut_label=%ARG_LABEL%
