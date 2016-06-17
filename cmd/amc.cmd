1248254e1d26f087da61d15a1c053dfec01ffac1
Reinhard Pointner <reinhard.pointner@gmail.com>
2016-06-17 12:44:53 +0800

    Windows CMD droplet for the AMC Script

---
+++ cmd/amc.cmd
@@ -0,0 +1,1 @@
+filebot -script fn:amc --output "%USERPROFILE%/Media" --action duplicate --conflict skip -non-strict --log-file amc.log --def skipExtract=y unsorted=y music=y artwork=y excludeList=excludes.txt %*
