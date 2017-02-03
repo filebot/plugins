23f7ae6961fcade3f034653fe9b34bc8ede4e944
Reinhard Pointner <reinhard.pointner@gmail.com>
2017-02-03 18:29:26 +0800

    Experiment with docker

---
+++ docker/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -u
+
+for TAG in latest node watcher; do
+    curl -H "Content-Type: application/json" --data "{\"docker_tag\": \"$TAG\"}" -X POST https://registry.hub.docker.com/u/rednoah/filebot/trigger/06323d5b-28a2-4332-8f84-66587f27dea9/
+    sleep 5
+done
