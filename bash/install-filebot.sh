259f8ad889ddbd2def14a48d3a9dfe9ee0928bc0
Reinhard Pointner <reinhard.pointner@gmail.com>
2017-02-06 04:39:58 +0800

    sha256sum --check

--- bash/install-filebot.sh
+++ bash/install-filebot.sh
@@ -1,10 +1,20 @@
 #!/bin/sh -xu
 
+FILEBOT_VERSION=4.7.5
+FILEBOT_SHA256=bb305d11922dbac3d7551177a45cab9e24c043a1993f5a8b160c887f317d0429
+
+FILEBOT_PACKAGE=FileBot_$FILEBOT_VERSION-portable.tar.xz
+FILEBOT_PACKAGE_URL=https://sourceforge.net/projects/filebot/files/filebot/FileBot_$FILEBOT_VERSION/$FILEBOT_PACKAGE
+
+
 # Download latest portable package
-curl -v -L -o "FileBot-portable.tar.xz" "https://sourceforge.net/projects/filebot/files/filebot/HEAD/FileBot_4.7.8_B2/FileBot_4.7.8-portable.tar.xz"
+curl -sL -O "$FILEBOT_PACKAGE_URL"
+
+# Check SHA-256 checksum
+echo "$FILEBOT_SHA256 *$FILEBOT_PACKAGE" | sha256sum --check --strict || exit 1
 
 # Extract *.tar.xz archive
-tar xvf "FileBot-portable.tar.xz"
+tar xvf "$FILEBOT_PACKAGE"
 
 # Check if filebot.sh works
 "$PWD/filebot.sh" -script fn:sysinfo
