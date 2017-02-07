da0671f15871104cdca6fb28e046565ac45fdc55
Reinhard Pointner <reinhard.pointner@gmail.com>
2017-02-07 19:57:13 +0800

    Use https://downloads.sourceforge.net/project/filebot/

--- bash/install-filebot.sh
+++ bash/install-filebot.sh
@@ -4,7 +4,7 @@ FILEBOT_VERSION=4.7.7
 FILEBOT_SHA256=bb305d11922dbac3d7551177a45cab9e24c043a1993f5a8b160c887f317d0429
 
 FILEBOT_PACKAGE=FileBot_$FILEBOT_VERSION-portable.tar.xz
-FILEBOT_PACKAGE_URL=https://sourceforge.net/projects/filebot/files/filebot/FileBot_$FILEBOT_VERSION/$FILEBOT_PACKAGE
+FILEBOT_PACKAGE_URL=https://downloads.sourceforge.net/project/filebot/filebot/FileBot_$FILEBOT_VERSION/$FILEBOT_PACKAGE
 
 
 # Download latest portable package
