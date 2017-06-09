ebc6c94c47c804aa878c936580b38248836dc0a1
Reinhard Pointner <reinhard.pointner@gmail.com>
2017-06-10 00:57:37 +0800

    Update portable.sh

--- brew/filebot.rb
+++ brew/filebot.rb
@@ -3,7 +3,7 @@ cask 'filebot' do
   sha256 '6a97eed72a11bc16637e5f7e969d49cd6112b7bb2eee90d623e1574ade833ae5'
 
   url "https://get.filebot.net/filebot/#{version}/FileBot_#{version}-darwin.tar.xz.gpg"
-  gpg :embedded, key_url: 'https://raw.githubusercontent.com/filebot/plugins/master/brew/filebot.pub'
+  gpg :embedded, key_url: 'https://raw.githubusercontent.com/filebot/plugins/master/gpg/maintainer.pub'
   appcast 'https://app.filebot.net/update.xml', checkpoint: 'cfc707d5378dd6e9ca479a4d20f92801e4a18ce326909ff31b99d417649a4fae'
 
   name 'FileBot'
