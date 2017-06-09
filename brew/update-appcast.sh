e025beafb34b0707d1344af3ccb31ae4ce3916be
Reinhard Pointner <reinhard.pointner@gmail.com>
2017-06-09 23:24:11 +0800

    Add experimental brew cask descriptor

---
+++ brew/update-appcast.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+URL='https://app.filebot.net/update.xml'
+SHA=`curl --compressed --location --silent "$URL" | shasum --algorithm 256 | cut -d' ' -f 1`
+
+sed -i '' -E "s/checkpoint: '.+'/checkpoint: '$SHA'/g"  filebot.rb
