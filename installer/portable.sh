f09efef8a5a58c1a1489bfc1982bebd167ab6557
Reinhard Pointner <reinhard.pointner@gmail.com>
2017-06-10 02:10:53 +0800

    Update portable.sh

--- installer/portable.sh
+++ installer/portable.sh
@@ -1,11 +1,15 @@
 #!/bin/sh -xu
 
-FILEBOT_VERSION=4.7.9
-FILEBOT_SHA256=2531bc67d3522b1db05870da2b72fff580ebfa5969655e75382ffe2c8dec6867
+FILEBOT_VERSION=4.7.10
+FILEBOT_SHA256=8ade6995e33db309a9aead54bf0f06f70d29d99425278719748dbf62b479cd81
 
-FILEBOT_PACKAGE=FileBot_$FILEBOT_VERSION-portable.tar.xz
-FILEBOT_PACKAGE_URL=https://app.filebot.net/files/filebot/FileBot_$FILEBOT_VERSION/$FILEBOT_PACKAGE
+FILEBOT_PACKAGE=FileBot_$FILEBOT_VERSION-portable.tar.xz.gpg
+FILEBOT_PACKAGE_URL=https://get.filebot.net/filebot/$FILEBOT_VERSION/$FILEBOT_PACKAGE
+FILEBOT_PACKAGE_KEY=4E402EBF7C3C6A71
 
+
+# initialize GnuPG
+gpg --recv-keys "$FILEBOT_PACKAGE_KEY"
 
 # Download latest portable package
 curl -L -O "$FILEBOT_PACKAGE_URL"
@@ -13,8 +17,11 @@ curl -L -O "$FILEBOT_PACKAGE_URL"
 # Check SHA-256 checksum
 echo "$FILEBOT_SHA256 *$FILEBOT_PACKAGE" | sha256sum --check || exit 1
 
+# verify GnuPG signature
+gpg --batch --yes --trusted-key "$FILEBOT_PACKAGE_KEY" --output "FileBot.tar.xz" --decrypt "$FILEBOT_PACKAGE" || exit 1
+
 # Extract *.tar.xz archive
-tar xvf "$FILEBOT_PACKAGE"
+tar xvf "FileBot.tar.xz" && rm "FileBot.tar.xz" "$FILEBOT_PACKAGE"
 
 # Check if filebot.sh works
 "$PWD/filebot.sh" -script fn:sysinfo
