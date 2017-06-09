cfa200225344ac3165f0dbc375c93cab3ff87e03
Reinhard Pointner <reinhard.pointner@gmail.com>
2017-06-09 15:43:24 +0800

    Update subtitles.sh

--- installer/subtitles.sh
+++ installer/subtitles.sh
@@ -1,11 +1,13 @@
 #!/bin/sh -xu
 
-PKG_URL="https://get.filebot.net/filebot/latest/FileBot_Subtitles.pkg"
-PKG="/tmp/FileBot_Subtitles.pkg"
+
+URL="https://get.filebot.net/filebot/latest/FileBot_Subtitles.pkg"
+PKG="${TMPDIR}FileBot_Subtitles.pkg"
 APP="/Applications/FileBot Subtitles.app"
 
+
 function fetch {
-	curl --fail --location --output "$1" --time-cond "$1" --retry 5 "$2"
+	curl --verbose --fail --location --output "$1" --time-cond "$1" --retry 5 "$2"
 }
 
 function verify {
@@ -16,4 +18,5 @@ function install {
 	sudo installer -verbose -package "$1" -target LocalSystem
 }
 
-fetch "$PKG" "$PKG_URL" && verify "$PKG" && install "$PKG" && open --reveal "$APP"
+
+fetch "$PKG" "$URL" && verify "$PKG" && install "$PKG" && open --reveal "$APP"
