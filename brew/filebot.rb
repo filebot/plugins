37d9ce29e229fc4c0e57a9d9c691fe6bfa3239f1
Reinhard Pointner <reinhard.pointner@gmail.com>
2017-06-09 22:52:23 +0800

    Add experimental brew cask descriptor

--- brew/filebot.rb
+++ brew/filebot.rb
@@ -3,7 +3,7 @@ cask 'filebot' do
   sha256 '6a97eed72a11bc16637e5f7e969d49cd6112b7bb2eee90d623e1574ade833ae5'
 
   url "https://get.filebot.net/filebot/#{version}/FileBot_#{version}-darwin.tar.xz.gpg"
-  gpg :embedded, key_id: 'b0976e51e5c047ad0fd051294e402ebf7c3c6a71'
+  gpg :embedded, key_url: 'https://raw.githubusercontent.com/filebot/plugins/master/brew/filebot.pub'
   appcast 'https://app.filebot.net/update.xml', checkpoint: 'cfc707d5378dd6e9ca479a4d20f92801e4a18ce326909ff31b99d417649a4fae'
 
   name 'FileBot'
