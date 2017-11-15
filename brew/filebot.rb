c4d57b92f4ff76d85b282ccd9d7640a96bb6e50f
Reinhard Pointner <reinhard.pointner@gmail.com>
2017-11-15 21:42:59 +0100

    Update installer scripts

--- brew/filebot.rb
+++ brew/filebot.rb
@@ -1,8 +1,9 @@
 cask 'filebot' do
-  version '4.7.10'
+  version '4.7.14'
   sha256 '6a97eed72a11bc16637e5f7e969d49cd6112b7bb2eee90d623e1574ade833ae5'
 
-  url "https://get.filebot.net/filebot/#{version}/FileBot_#{version}-darwin.tar.xz.gpg"
+  url "https://get.filebot.net/filebot/latest/FileBot_4.7.14_r5241-darwin.tar.xz.gpg"
+  # url "https://get.filebot.net/filebot/#{version}/FileBot_#{version}-darwin.tar.xz.gpg"
   gpg :embedded, key_url: 'https://raw.githubusercontent.com/filebot/plugins/master/gpg/maintainer.pub'
   appcast 'https://app.filebot.net/update.xml', checkpoint: 'cfc707d5378dd6e9ca479a4d20f92801e4a18ce326909ff31b99d417649a4fae'
 
