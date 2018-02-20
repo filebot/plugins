63830d2703d1965c8edc62c4d0ed3dba2ebdcc04
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-02-20 12:45:59 +0700

    Deploy dedicated cask package

--- brew/filebot.rb
+++ brew/filebot.rb
@@ -1,16 +1,15 @@
 cask 'filebot' do
-  version '4.7.14'
-  sha256 'c9dc41d2a47d9db99b5ec8e18603805bf1809964e79f9ffbe2e44f0129f320ff'
+  version '4.7.15.9'
+  sha256 '13dc465bf4ad6930af8d6656741e69a52f6aee266fe56b03ea1af72163ce006c'
 
-  url "https://get.filebot.net/filebot/latest/FileBot_4.7.14_r5241-darwin.tar.xz.gpg"
+  url "https://get.filebot.net/filebot/latest/FileBot_#{version}-darwin.tar.xz.gpg"
   gpg "#{url}.asc", key_url: 'https://raw.githubusercontent.com/filebot/plugins/master/gpg/maintainer.pub'
   appcast 'https://app.filebot.net/update.xml', checkpoint: 'cfc707d5378dd6e9ca479a4d20f92801e4a18ce326909ff31b99d417649a4fae'
 
   name 'FileBot'
   homepage 'https://www.filebot.net/'
 
-  binary 'FileBot.app/Contents/MacOS/filebot.sh', target: 'filebot'
-  zap delete: '~/Library/Preferences/net.filebot.ui.plist'
+  binary 'filebot.sh', target: 'filebot'
 
   depends_on formula: 'gpg'
   depends_on cask: 'java'
