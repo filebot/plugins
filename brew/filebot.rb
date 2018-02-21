59eff80cc0938adfb72e7711418a686c0f9c3924
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-02-21 11:53:35 +0700

    gpg :embedded

--- brew/filebot.rb
+++ brew/filebot.rb
@@ -3,7 +3,7 @@ cask 'filebot' do
   sha256 '13dc465bf4ad6930af8d6656741e69a52f6aee266fe56b03ea1af72163ce006c'
 
   url "https://get.filebot.net/filebot/latest/FileBot_#{version}-darwin.tar.xz.gpg"
-  gpg "#{url}.asc", key_url: 'https://raw.githubusercontent.com/filebot/plugins/master/gpg/maintainer.pub'
+  gpg :embedded, key_url: 'https://raw.githubusercontent.com/filebot/plugins/master/gpg/maintainer.pub'
   appcast 'https://app.filebot.net/update.xml', checkpoint: 'cfc707d5378dd6e9ca479a4d20f92801e4a18ce326909ff31b99d417649a4fae'
 
   name 'FileBot'
