b780c354547fc790314aaf1df4e7ad60072b5c31
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-03-08 10:58:00 +0700

    Update Linux package for JDK 9

--- installer/portable.sh
+++ installer/portable.sh
@@ -1,12 +1,8 @@
 #!/bin/sh -xu
 
-FILEBOT_VERSION=4.7.14
-FILEBOT_SHA256=65d2bfe425632c97d8be67f244676d88239c011ab76fb58f3a52e6b3b0e9dcb4
-
-FILEBOT_PACKAGE=FileBot_4.7.14_r5241-portable.tar.xz.gpg
+FILEBOT_PACKAGE=FileBot.tar.xz.gpg
 FILEBOT_PACKAGE_URL=http://get.filebot.net/filebot/latest/$FILEBOT_PACKAGE
-# FILEBOT_PACKAGE=FileBot_$FILEBOT_VERSION-portable.tar.xz.gpg
-# FILEBOT_PACKAGE_URL=https://get.filebot.net/filebot/$FILEBOT_VERSION/$FILEBOT_PACKAGE
+FILEBOT_PACKAGE_SHA256=6c68e09a60f4f9aaff6e5ebd93e7f284bb2204ab0314f0635a355494916c7935
 FILEBOT_PACKAGE_KEY=4E402EBF7C3C6A71
 FILEBOT_PACKAGE_KEY_URL=https://raw.githubusercontent.com/filebot/plugins/master/gpg/maintainer.pub
 
@@ -15,7 +11,7 @@ FILEBOT_PACKAGE_KEY_URL=https://raw.githubusercontent.com/filebot/plugins/master
 curl -o "$FILEBOT_PACKAGE" -z "$FILEBOT_PACKAGE" --retry 5 --location "$FILEBOT_PACKAGE_URL"
 
 # Check SHA-256 checksum
-echo "$FILEBOT_SHA256 *$FILEBOT_PACKAGE" | sha256sum --check || exit 1
+echo "$FILEBOT_PACKAGE_SHA256 *$FILEBOT_PACKAGE" | sha256sum --check || exit 1
 
 # initialize GnuPG
 curl -fsSL "$FILEBOT_PACKAGE_KEY_URL" | gpg --import
