b41835266cb2c11be6f422e202cb2aac2fa67b91
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-03-15 18:11:15 +0700

    Dear Linux user, `whoami`,
    FileBot currently only officially supports Windows 10 and macOS because these platforms make it easy to sell FileBot via their respective app stores. Unfortunately, selling software on Ubuntu, Debian, Red Hat, SUSE, Synology NAS, QNAP NAS, etc is not as easy and not really worth the effort considering the small number of users willing to support the project. This Patreon is an experiment to see how many users are willing to financially contribute towards freely available donation-supported new releases on these platforms.
    
    Please support FileBot for Linux with a pledge of \$1 per release on Patreon:
    
    => https://www.patreon.com/filebot
    
    If every other Linux user were to pledge a small amount of money to the FileBot project, then you could easily fund the project ten times over. Please financially support the software you use and rely on, be it FileBot or any other tool. It's just polite, and it helps the ecosystem.
    
    Thank you,
    The FileBot Team
    
    // https://github.com/filebot/plugins/issues/11

--- installer/portable.sh
+++ installer/portable.sh
@@ -1,5 +1,27 @@
-#!/bin/sh -xu
+#!/bin/sh
 
+
+
+# Advertise FileBot for Linux Patreon page
+echo "Dear Linux user, `whoami`,
+
+FileBot currently only officially supports Windows 10 and macOS because these platforms make it easy to sell FileBot via their respective app stores. Unfortunately, selling software on Ubuntu, Debian, Red Hat, SUSE, Synology NAS, QNAP NAS, etc is not as easy and not really worth the effort considering the small number of users willing to support the project. This Patreon is an experiment to see how many users are willing to financially contribute towards freely available donation-supported new releases on these platforms.
+
+Please support FileBot for Linux with a pledge of \$1 per release on Patreon:
+
+=> https://www.patreon.com/filebot
+
+If every other Linux user were to pledge a small amount of money to the FileBot project, then you could easily fund the project ten times over. Please financially support the software you use and rely on, be it FileBot or any other tool. It's just polite, and it helps the ecosystem.
+
+Thank you,
+The FileBot Team
+"
+
+exit 1
+
+
+
+# TODO: release new version of FileBot for Linux
 FILEBOT_PACKAGE_VERSION=4.7.21
 FILEBOT_PACKAGE_SHA256=e7c99f5a0b55bf52d78f94fec3344f1951c70e0ad33aa2bc51596863fff90fc8
 
