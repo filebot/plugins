66fa50665139b422e4874aec0c36e43f7f27615a
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-06-25 20:20:45 +0700

    Test new app bundle builds with brew cask

--- brew/filebot.rb
+++ brew/filebot.rb
@@ -1,16 +1,20 @@
 cask 'filebot' do
-  version '4.7.21'
-  sha256 '8b1d2b715e50133e0d6faab6674fca7f8f403c81ed9f4d2cd8449935f9287442'
+  version '4.8.2'
+  sha256 'b226b6f2b98417d7b4f452af927b39557adfceabb7db0b0b082c6c7e126a5f74'
 
-  url "https://get.filebot.net/filebot/latest/FileBot_#{version}-darwin.tar.xz.gpg"
-  gpg :embedded, key_url: 'https://raw.githubusercontent.com/filebot/plugins/master/gpg/maintainer.pub'
-  appcast 'https://app.filebot.net/update.xml', checkpoint: 'cfc707d5378dd6e9ca479a4d20f92801e4a18ce326909ff31b99d417649a4fae'
+  url "https://get.filebot.net/filebot/FileBot_#{version}/FileBot_#{version}.app.tar.xz"
+  gpg "#{url}.asc", key_url: 'https://raw.githubusercontent.com/filebot/plugins/master/gpg/maintainer.pub'
+  appcast 'https://app.filebot.net/update.xml'
 
   name 'FileBot'
   homepage 'https://www.filebot.net/'
 
-  binary 'filebot.sh', target: 'filebot'
+  app "FileBot.app"
+  binary "#{appdir}/FileBot.app/Contents/MacOS/filebot.sh", target: 'filebot'
 
-  depends_on formula: 'gpg'
-  depends_on cask: 'java'
+  zap trash: '~/Library/Preferences/net.filebot.ui.plist'
+
+  caveats do
+    depends_on_java '10+'
+  end
 end
