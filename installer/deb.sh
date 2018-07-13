2c5db7538128b806a543d3825108c6e468e8236c
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-07-13 20:37:12 +0700

    Retrieve GPG key from GitHub repo

--- installer/deb.sh
+++ installer/deb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xu
 
 # 1. Import signing keys
-sudo apt-key add <<< "$(curl -fsSL https://get.filebot.net/filebot/KEYS)"
+sudo apt-key add <<< "$(curl -fsSL https://raw.githubusercontent.com/filebot/plugins/master/gpg/maintainer.pub)"
 
 # 2. Add deb repository to sources.list
 sudo echo "deb [arch=amd64] https://get.filebot.net/deb/ stable main" > /etc/apt/sources.list.d/filebot.list
