a6049f71a2aaced847fcc3a511b0773be4a428c8
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-07-13 19:03:24 +0700

    APT repository

--- installer/deb.sh
+++ installer/deb.sh
@@ -1,5 +1,13 @@
 #!/bin/bash -xu
-apt-key add <<< "$(curl -fsSL https://get.filebot.net/filebot/KEYS)"
-echo "deb [arch=amd64] https://get.filebot.net/deb/ stable main" > /etc/apt/sources.list.d/filebot.list
-apt-get update
-apt-get install filebot --install-suggests
+
+# 1. Import signing keys
+sudo apt-key add <<< "$(curl -fsSL https://get.filebot.net/filebot/KEYS)"
+
+# 2. Add deb repository to sources.list
+sudo echo "deb [arch=amd64] https://get.filebot.net/deb/ stable main" > /etc/apt/sources.list.d/filebot.list
+
+# 3. Update package index
+sudo apt-get update
+
+# 4. Install FileBot and its dependencies
+sudo apt-get install filebot --install-suggests
