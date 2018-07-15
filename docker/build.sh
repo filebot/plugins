618477a9391a6d7ffe039d31f65366770fb8dd06
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-07-15 22:38:48 +0700

    Use makefile

--- docker/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker build --rm -t filebot -f Dockerfile . && docker run -it -v $PWD:/volume1 -v data:/data filebot -script fn:sysinfo
--- docker/clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm -r input output
-
-docker kill $(docker ps -q)
-docker rm $(docker ps -a -q)
-docker rmi -f $(docker images -q)
