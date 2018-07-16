0d5091098acb81f911b746d4955c3e7751526f4f
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-07-16 22:06:58 +0700

    Update filebot.rb

--- brew/update-appcast.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-URL='https://app.filebot.net/update.xml'
-SHA=`curl --compressed --location --silent "$URL" | shasum --algorithm 256 | cut -d' ' -f 1`
-
-sed -i '' -E "s/checkpoint: '.+'/checkpoint: '$SHA'/g" filebot.rb
