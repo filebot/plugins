c9a4e83f0b91398ba6fef245bc98c994ee815e75
Reinhard Pointner <reinhard.pointner@gmail.com>
2019-04-05 17:18:15 +0700

    deb https://get.filebot.net/deb/ universal main

--- installer/deb.sh
+++ installer/deb.sh
@@ -4,7 +4,7 @@
 sudo apt-key adv --fetch-keys "https://raw.githubusercontent.com/filebot/plugins/master/gpg/maintainer.pub"
 
 # 2. Add deb repository to sources.list
-echo "deb [arch=amd64] https://get.filebot.net/deb/ stable main" | sudo tee /etc/apt/sources.list.d/filebot.list
+echo "deb https://get.filebot.net/deb/ universal main" | sudo tee /etc/apt/sources.list.d/filebot.list
 
 # 3. Update package index
 sudo apt-get update
