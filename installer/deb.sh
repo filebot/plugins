cfc1a0a9fc186ebb8864611f2c5c8b6fbc099a24
Reinhard Pointner <rednoah@filebot.net>
2019-04-09 16:21:24 +0700

    # use JDK 11  build for Debian Buster / Ubuntu 18.10 or higher, and JDK 8 build otherwise

--- installer/deb.sh
+++ installer/deb.sh
@@ -1,10 +1,17 @@
 #!/bin/sh -xu
 
+# use JDK 11  build for Debian Buster / Ubuntu 18.10 or higher, and JDK 8 build otherwise
+if apt-cache show openjfx | grep "Version: 8"; then
+	REPO="deb https://get.filebot.net/deb/ universal-jdk8 main"
+else
+	REPO="deb https://get.filebot.net/deb/ universal main"
+fi
+
 # 1. Import signing keys
 sudo apt-key adv --fetch-keys "https://raw.githubusercontent.com/filebot/plugins/master/gpg/maintainer.pub"
 
 # 2. Add deb repository to sources.list
-echo "deb https://get.filebot.net/deb/ universal main" | sudo tee /etc/apt/sources.list.d/filebot.list
+echo "$REPO" | sudo tee /etc/apt/sources.list.d/filebot.list
 
 # 3. Update package index
 sudo apt-get update
