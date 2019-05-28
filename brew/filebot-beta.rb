8df17e379edd2bedbabbe8f8ef0bed1d83edf0f8
Reinhard Pointner <reinhard.pointner@gmail.com>
2019-05-28 14:42:54 +0700

    appcast "#{url}.sha256"

--- brew/filebot-beta.rb
+++ brew/filebot-beta.rb
@@ -3,10 +3,11 @@ cask 'filebot-beta' do
   sha256 :no_check
 
   url "https://get.filebot.net/filebot/BETA/FileBot_4.8.6.app.tar.xz"
+  gpg "#{url}.asc", key_url: 'https://raw.githubusercontent.com/filebot/plugins/master/gpg/maintainer.pub'
+  appcast "#{url}.sha256"
 
   name 'FileBot'
   homepage 'https://www.filebot.net/'
-  gpg "#{url}.asc", key_url: 'https://raw.githubusercontent.com/filebot/plugins/master/gpg/maintainer.pub'
 
   app 'FileBot.app'
   binary "#{appdir}/FileBot.app/Contents/MacOS/filebot.sh", target: 'filebot'
