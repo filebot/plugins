0a92fed90c7f43ee20c6de9b69194258b82d63a4
Reinhard Pointner <reinhard.pointner@gmail.com>
2019-07-24 19:40:13 +0700

    Plex Xattr Agent

--- plex/Xattr.bundle/Contents/Code/__init__.py
+++ plex/Xattr.bundle/Contents/Code/__init__.py
@@ -1,34 +1,28 @@
-# local xattr agent
+
+
+def Start():
+  Log("START".ljust(157, '-'))
 
 
 #####################################################################################################################
 
 
-class xattrMovie(Agent.Movies):
-  name = 'Xattr (Movies)'
+class XattrMovieAgent(Agent.Movies):
+  name = 'FileBot Xattr Metadata (Movies)'
   languages = [Locale.Language.NoLanguage]
-  
-  def search(self, results, media, lang):
-    results.Append(MetadataSearchResult(id = 'null', score = 100))
+  primary_provider = False
+  accepts_from = ['com.plexapp.agents.localmedia']
 
-  def update(self, metadata, media, lang):
+  def search(self, results, media, lang):
+    Log("search()".ljust(157, '-'))
     Log("self: %s" % self)
-    Log("metadata: %s" % metadata)
+    Log("results: %s" % results)
     Log("media: %s" % media)
     Log("lang: %s" % lang)
-
-
-#####################################################################################################################
-
-
-class xattrSeries(Agent.TV_Shows):
-  name = 'Xattr (TV)'
-  languages = [Locale.Language.NoLanguage]
-
-  def search(self, results, media, lang):
     results.Append(MetadataSearchResult(id = 'null', score = 100))
 
   def update(self, metadata, media, lang):
+    Log("update()".ljust(157, '-'))
     Log("self: %s" % self)
     Log("metadata: %s" % metadata)
     Log("media: %s" % media)
