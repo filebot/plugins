d815ff618acbb01e7368d1b09c9154add18112ef
Reinhard Pointner <reinhard.pointner@gmail.com>
2019-07-27 14:05:56 +0700

    #define EXTATTR_NAMESPACE_USER    0x00000001

--- plex/Xattr.bundle/Contents/Code/xattr.py
+++ plex/Xattr.bundle/Contents/Code/xattr.py
@@ -37,7 +37,7 @@ elif os.name == 'posix':
     libc.extattr_get_file.argtypes = (c_char_p, c_int, c_char_p, c_char_p, c_size_t)
     libc.extattr_get_file.restype = c_ssize_t
     def getxattr_impl(file, name, buffer):
-      return libc.extattr_get_file(file, 0x0001, name, buffer, sizeof(buffer))
+      return libc.extattr_get_file(file, 0x0001, name, buffer, sizeof(buffer))  #define EXTATTR_NAMESPACE_USER    0x00000001
 
   else:
     def getxattr_impl(file, name, buffer):
