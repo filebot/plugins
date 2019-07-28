ea561dd55cad3a9e1b458460a3449f8137eaf645
Reinhard Pointner <reinhard.pointner@gmail.com>
2019-07-29 03:53:28 +0700

    xattr accessors

--- plex/Xattr.bundle/Contents/Code/filebot.py
+++ plex/Xattr.bundle/Contents/Code/filebot.py
@@ -5,6 +5,9 @@ from datetime import *
 from xattr import *
 
 
+#####################################################################################################################
+
+
 def xattr_metadata(file):
   Log("[FILE] %s" % file)
 
@@ -22,57 +25,6 @@ def xattr_metadata(file):
     return None
 
 
-def movie_id(attr):
-  id = attr['tmdbId']
-  if id > 0:
-    Log("[TMDB] %s" % id)
-    return str(id)
-
-  id = attr['imdbId']
-  if id > 0:
-    id = 'tt%07d' % id
-    Log("[IMDB] %s" % id)
-    return id
-
-  return None
-
-
-def thetvdb_id(attr):
-  if attr_get(attr, 'seriesInfo', 'database') == 'TheTVDB':
-    id = attr_get(attr, 'seriesInfo', 'id')
-    Log("[TheTVDB] %s" % id)
-    return str(id)
-  return None
-
-
-def series_name(attr):
-  return attr_get(attr, 'seriesName')
-
-def series_year(attr):
-  return attr_get(attr, 'seriesInfo', 'startDate', 'year')
-
-def series_language(attr):
-  return attr_get(attr, 'seriesInfo', 'language')
-
-def series_date(attr):
-  return attr_date(attr_get(attr, 'seriesInfo', 'startDate'))
-
-def episode_date(attr):
-  return attr_date(attr_get(attr, 'airdate'))
-
-def attr_get(attr, *keys):
-  for k in keys:
-    attr = attr.get(k)
-    if attr is None:
-      return None
-  return attr
-
-def attr_date(attr):
-  if attr is not None:
-    return datetime(year=attr['year'], month=attr['month'], day=attr['day'])
-  return None
-
-
 def getxattr_plain_file(store, file, name):
   if not isabs(store):
     store = join(dirname(file), store)
@@ -88,3 +40,69 @@ def getxattr_plain_file(store, file, name):
   buffer = fd.read()
   fd.close()
   return buffer.decode('UTF-8')
+
+
+#####################################################################################################################
+
+
+def movie_id(attr):
+  id = attr['tmdbId']
+  if id > 0:
+    Log("[TMDB] %s" % id)
+    return str(id)
+
+  id = attr['imdbId']
+  if id > 0:
+    id = 'tt%07d' % id
+    Log("[IMDB] %s" % id)
+    return id
+
+  return None
+
+
+def movie_name(attr):     return attr.get('name')
+def movie_year(attr):     return attr.get('year')
+def movie_language(attr): return attr.get('language')
+
+
+#####################################################################################################################
+
+
+def tvdb_series_id(attr):
+  if attr_get(attr, 'seriesInfo', 'database') == 'TheTVDB':
+    id = attr_get(attr, 'seriesInfo', 'id')
+    Log("[TheTVDB] %s" % id)
+    return str(id)
+  return None
+
+
+def series_name(attr): return attr_get(attr, 'seriesName')
+def series_year(attr): return attr_get(attr, 'seriesInfo', 'startDate', 'year')
+
+
+def series_language(attr):      return attr_get(attr, 'seriesInfo', 'language')
+def series_date(attr):          return attr_date(attr_get(attr, 'seriesInfo', 'startDate'))
+def series_certification(attr): return attr_get(attr, 'seriesInfo', 'certification')
+def series_network(attr):       return attr_get(attr, 'seriesInfo', 'network')
+def series_runtime(attr):       return attr_get(attr, 'seriesInfo', 'runtime')
+def series_rating(attr):        return attr_get(attr, 'seriesInfo', 'rating')
+def series_genres(attr):        return attr_get(attr, 'seriesInfo', 'genres')
+
+
+def episode_title(attr):           return attr.get('title')
+def episode_absolute_number(attr): return attr.get('absolute')
+def episode_date(attr):            return attr_date(attr_get(attr, 'airdate'))
+
+
+def attr_get(attr, *keys):
+  for k in keys:
+    attr = attr.get(k)
+    if attr is None:
+      return None
+  return attr
+
+
+def attr_date(attr):
+  if attr is not None:
+    return datetime(year=attr['year'], month=attr['month'], day=attr['day'])
+  return None
