ec5b4544e8e04e2796f4dac199a20f6ceaeb5163
Reinhard Pointner <reinhard.pointner@gmail.com>
2019-07-29 23:09:47 +0700

    Fix xattr on Linux

--- plex/Plug-ins/Xattr.bundle/Contents/Code/xattr.py
+++ plex/Plug-ins/Xattr.bundle/Contents/Code/xattr.py
@@ -5,7 +5,7 @@ import os, sys
 if os.name == 'nt':
     def getxattr(file, name):
       try:
-        fd = open("%s:%s" % (file, name), "rb")
+        fd = open("%s:%s" % (file, name), 'rb')
         buffer = fd.read()
         fd.close()
         return buffer.decode('UTF-8')
@@ -25,28 +25,25 @@ elif os.name == 'posix':
     libc.getxattr.argtypes = (c_char_p, c_char_p, c_char_p, c_size_t)
     libc.getxattr.restype = c_ssize_t
     def getxattr_impl(file, name, buffer):
-      return libc.getxattr(file, name, buffer, sizeof(buffer))
+      return libc.getxattr(fsencode(file), fsencode('user.'+name), buffer, sizeof(buffer))
 
   elif sys.platform.find('darwin') >= 0:
     libc.getxattr.argtypes = (c_char_p, c_char_p, c_char_p, c_size_t, c_uint32, c_int)
     libc.getxattr.restype = c_ssize_t
     def getxattr_impl(file, name, buffer):
-      return libc.getxattr(file, name, buffer, sizeof(buffer), 0, 0)
+      return libc.getxattr(fsencode(file), fsencode(name), buffer, sizeof(buffer), 0, 0)
 
   elif sys.platform.find('bsd') >= 0:
     libc.extattr_get_file.argtypes = (c_char_p, c_int, c_char_p, c_char_p, c_size_t)
     libc.extattr_get_file.restype = c_ssize_t
     def getxattr_impl(file, name, buffer):
-      return libc.extattr_get_file(file, 0x0001, name, buffer, sizeof(buffer))  #define EXTATTR_NAMESPACE_USER    0x00000001
+      return libc.extattr_get_file(fsencode(file), 0x0001, fsencode(name), buffer, sizeof(buffer))  #define EXTATTR_NAMESPACE_USER    0x00000001
 
 
   def fsencode(file):
     return file.encode(sys.getfilesystemencoding())
 
   def getxattr(file, name):
-    file = fsencode(file)
-    name = fsencode(name)
-
     buffer = create_string_buffer(8 * 1024)  # xattr size may be limited to 4 KB or less (e.g. ext4)
     n = getxattr_impl(file, name, buffer)
     if n > 0:
