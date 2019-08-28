de9b20d5640d4dbee959ba3a42bd801227c409cd
Reinhard Pointner <reinhard.pointner@gmail.com>
2019-08-28 15:18:41 +0700

    Link bash completion

--- brew/filebot-beta.rb
+++ brew/filebot-beta.rb
@@ -3,7 +3,7 @@ cask 'filebot-beta' do
   sha256 :no_check
 
   url "https://get.filebot.net/filebot/BETA/FileBot_4.8.6.app.tar.xz"
-  gpg "#{url}.asc", key_url: 'https://raw.githubusercontent.com/filebot/plugins/master/gpg/maintainer.pub'
+  # gpg "#{url}.asc", key_url: 'https://raw.githubusercontent.com/filebot/plugins/master/gpg/maintainer.pub'
   appcast "#{url}.sha256"
 
   name 'FileBot'
@@ -11,6 +11,7 @@ cask 'filebot-beta' do
 
   app 'FileBot.app'
   binary "#{appdir}/FileBot.app/Contents/MacOS/filebot.sh", target: 'filebot'
+  binary "#{appdir}/FileBot.app/Contents/Resources/bash_completion.d/filebot", target: "#{HOMEBREW_PREFIX}/etc/bash_completion.d/filebot"
 
   zap trash: '~/Library/Preferences/net.filebot.ui.plist'
 end
