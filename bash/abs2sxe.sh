6045c18d87de2401ed0a50467f33019361358d8d
Reinhard Pointner <reinhard.pointner@gmail.com>
2020-06-02 13:12:16 +0700

    Add --mode interactive by default

--- bash/abs2sxe.sh
+++ bash/abs2sxe.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -xu
-filebot -rename --db TheTVDB --order Airdate --mapper "order.absolute.episode" --format "{plex.name}" -non-strict "$@"
+filebot -rename --db TheTVDB --order Airdate --mode interactive --mapper "order.absolute.episode" --format "{plex.name}" -non-strict "$@"
