89cac7714245d524d4577b35c780ccffd25ed400
Reinhard Pointner <reinhard.pointner@gmail.com>
2020-06-02 13:17:24 +0700

    Update cask to FileBot 4.9.2

--- brew/filebot-latest.rb
+++ brew/filebot-latest.rb
@@ -2,7 +2,7 @@ cask 'filebot-beta' do
   version :latest
   sha256 :no_check
 
-  url "https://get.filebot.net/filebot/BETA/FileBot_4.8.6.app.tar.xz"
+  url "https://get.filebot.net/filebot/BETA/FileBot_4.8.2.app.tar.xz"
   # gpg "#{url}.asc", key_url: 'https://raw.githubusercontent.com/filebot/plugins/master/gpg/maintainer.pub'
   appcast "#{url}.sha256"
 
