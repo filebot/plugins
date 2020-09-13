46da24b84c66bf58462b6b2fab1b15443ddc5ccc
Reinhard Pointner <rednoah@users.noreply.github.com>
2020-09-13 10:48:01 +0700

    FileBot 4.9.2

--- brew/filebot-latest.rb
+++ brew/filebot-latest.rb
@@ -1,16 +1,17 @@
-cask 'filebot-latest' do
+cask "filebot-latest" do
   version :latest
   sha256 :no_check
 
-  url 'https://get.filebot.net/filebot/BETA/FileBot_4.8.2.app.tar.xz'
-  # gpg "#{url}.asc", key_url: 'https://raw.githubusercontent.com/filebot/plugins/master/gpg/maintainer.pub'
-  appcast "#{url}.sha256"
-  name 'FileBot'
-  homepage 'https://www.filebot.net/'
+  url "https://get.filebot.net/filebot/BETA/FileBot_4.9.2.app.tar.xz"
 
-  app 'FileBot.app'
-  binary "#{appdir}/FileBot.app/Contents/MacOS/filebot.sh", target: 'filebot'
-  binary "#{appdir}/FileBot.app/Contents/Resources/bash_completion.d/filebot_completion", target: "#{HOMEBREW_PREFIX}/etc/bash_completion.d/filebot"
+  name "FileBot"
+  desc "Tool for organizing and renaming movies, TV shows, anime or music"
+  homepage "https://www.filebot.net/"
 
-  zap trash: '~/Library/Preferences/net.filebot.ui.plist'
+  app "FileBot.app"
+  binary "#{appdir}/FileBot.app/Contents/MacOS/filebot.sh", target: "filebot"
+  binary "#{appdir}/FileBot.app/Contents/Resources/bash_completion.d/filebot_completion",
+         target: "#{HOMEBREW_PREFIX}/etc/bash_completion.d/filebot"
+
+  zap trash: "~/Library/Preferences/net.filebot.ui.plist"
 end
