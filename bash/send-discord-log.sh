8d18457a1edcc0e1b5d921444b00406916eaeb9f
Reinhard Pointner <rednoah@filebot.net>
2021-09-10 11:36:28 +0700

    send-discord-log.sh

---
+++ bash/send-discord-log.sh
@@ -0,0 +1,53 @@
+#!/bin/sh -u
+
+
+# Usage:
+# ./send-discord-log.sh filebot -script fn:sysinfo
+
+
+# required user configuration
+DISCORD_WEBHOOK_URL="<Copy Webhook URL>"
+
+# use command as task name
+TASK_NAME="$1"
+GREP_LINES="^\W|^Fail|^Skip"
+
+
+# create temporary files
+MSG_FILE=$(mktemp)
+LOG_FILE=$(mktemp)
+
+# delete temporary files on exit
+function finish {
+	rm "$LOG_FILE" "$MSG_FILE"
+}
+trap finish EXIT
+
+
+# run command and capture console output
+(time "$@") > "$LOG_FILE" 2>&1
+
+if [ $? -eq 0 ]; then
+	STATUS="OK"
+
+	# post to Discord only if the command failed (disabled by default)
+	# exit 0
+else
+	STATUS="FAILURE"
+fi
+
+echo "\`\`\`
+$STATUS | $(echo "$@")
+----------------------------------------
+$(egrep "$GREP_LINES" "$LOG_FILE")
+\`\`\`" >> "$MSG_FILE"
+
+# print raw message
+# cat "$MSG_FILE"
+
+# post to discord
+curl \
+	--form "username=$TASK_NAME" \
+	--form "content=<$MSG_FILE" \
+	--form "file=@$LOG_FILE;type=text/plain;filename=$TASK_NAME.log" \
+	"$DISCORD_WEBHOOK_URL"
