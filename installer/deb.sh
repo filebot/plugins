bf9997efe3a6388b584fc76f1e51374c1b1cce23
Reinhard Pointner <rednoah@filebot.net>
2022-11-11 16:18:03 +0700

    Call apt-get install -y for non-interactive use cases

--- installer/deb.sh
+++ installer/deb.sh
@@ -4,7 +4,7 @@
 uname -a
 
 # 0. Install pre-requisites
-sudo apt-get install --install-recommends dirmngr gnupg-curl apt-transport-https
+sudo apt-get install -y --install-recommends dirmngr gnupg-curl apt-transport-https
 
 # 1. Import signing keys
 curl -fsSL "https://raw.githubusercontent.com/filebot/plugins/master/gpg/maintainer.pub" | sudo gpg --dearmor --output "/usr/share/keyrings/filebot.gpg"
@@ -16,10 +16,10 @@ echo "deb [signed-by=/usr/share/keyrings/filebot.gpg] https://get.filebot.net/de
 sudo apt-get update
 
 # 4. Install dependencies explicitly (otherwise apt-get autoremove may purge them)
-sudo apt-get install --install-recommends default-jre openjfx zenity mediainfo libchromaprint-tools p7zip-full unrar
+sudo apt-get install -y --install-recommends default-jre openjfx zenity mediainfo libchromaprint-tools p7zip-full unrar
 
 # 5. Install FileBot
-sudo apt-get install --install-recommends filebot
+sudo apt-get install -y --install-recommends filebot
 
 # 6. Test Run
 filebot -script fn:sysinfo
