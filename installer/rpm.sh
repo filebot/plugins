ed8451662a25b1918a3ebb6cada0598ae306aa8d
Reinhard Pointner <rednoah@filebot.net>
2022-11-29 11:00:16 +0700

    Option A: Install FileBot via DNF Option B: Install FileBot via ZYPPER

--- installer/rpm.sh
+++ installer/rpm.sh
@@ -1,16 +1,36 @@
 #!/bin/sh -xu
 
-# 1. Add repository
-sudo dnf config-manager --add-repo 'https://raw.githubusercontent.com/filebot/plugins/master/yum/main.repo'
 
-# 2. Enable repository
-sudo dnf config-manager --set-enabled filebot --dump
+# Option A: Install FileBot via DNF
+if dnf --version 2> /dev/null
+then
+	# 1. Add repository
+	sudo dnf config-manager --add-repo 'https://raw.githubusercontent.com/filebot/plugins/master/yum/main.repo'
 
-# 3. Install dependencies
-sudo dnf install zenity mediainfo
+	# 2. Enable repository
+	sudo dnf config-manager --set-enabled filebot --dump
 
-# 4. Install FileBot
-sudo dnf install filebot
+	# 3. Install dependencies
+	sudo dnf install zenity mediainfo
 
-# 5. Test Run
+	# 4. Install FileBot
+	sudo dnf install filebot
+fi
+
+
+# Option B: Install FileBot via ZYPPER
+if zypper --version 2> /dev/null
+then
+	# 1. Add repository
+	sudo zypper addrepo --enable --refresh --gpgcheck --check 'https://raw.githubusercontent.com/filebot/plugins/master/yum/main.repo'
+
+	# 2. Install dependencies
+	sudo zypper install zenity mediainfo
+
+	# 3. Install FileBot
+	sudo zypper install filebot
+fi
+
+
+# Test Run
 filebot -script fn:sysinfo
