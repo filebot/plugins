afba35b6d339d6207c86a59c8def125f66461637
Reinhard Pointner <rednoah@filebot.net>
2023-05-23 13:23:36 +0700

    PACKAGE_VERSION=5.0.3

--- installer/tar-jdk8.sh
+++ installer/tar-jdk8.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -xu
 
-PACKAGE_VERSION=5.0.2
+PACKAGE_VERSION=5.0.3
 PACKAGE_SHA256=$(curl -fsSL https://raw.githubusercontent.com/filebot/website/master/get.filebot.net/filebot/FileBot_$PACKAGE_VERSION/FileBot_$PACKAGE_VERSION-portable-jdk8.tar.xz.sha256)
 
 PACKAGE_FILE=FileBot_$PACKAGE_VERSION-portable-jdk8.tar.xz
