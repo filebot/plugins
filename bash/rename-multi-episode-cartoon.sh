4585a8d3f24794395c899d92e33bfc0add7c88c1
Reinhard Pointner <rednoah@filebot.net>
2025-07-04 12:36:04 +0700

    Add rename-multi-episode-cartoon.sh (see https://www.filebot.net/forums/viewtopic.php?t=12644)

--- bash/rename-multi-episode-cartoon.sh
+++ bash/rename-multi-episode-cartoon.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -xu
-filebot -rename "$@" \
+filebot -rename \
   --db TheMovieDB::TV -non-strict \
   --format "{plex.name}" \
   --mapper "any{ if (regular) episode.number(null).map(episodelist.findAll{ airdate == it.airdate && !it.special }.episode).reverse() }{ episode.number(null) }" \
-  --mode interactive
+  --mode interactive \
+  "$@"
