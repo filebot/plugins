37f2495f417a5f3f5cd8e1d5e41ad7389779ce42
Reinhard Pointner <rednoah@filebot.net>
2026-01-15 13:17:05 +0700

    Add deb822-style .sources files

--- installer/deb.sh
+++ installer/deb.sh
@@ -7,19 +7,16 @@ uname -a
 sudo apt-get install -y --install-recommends dirmngr gnupg-curl apt-transport-https
 
 # 1. Import signing keys
-curl -fsSL "https://raw.githubusercontent.com/filebot/plugins/master/gpg/maintainer.pub" | sudo gpg --dearmor --output "/usr/share/keyrings/filebot.gpg"
+curl -o "/etc/apt/sources.list.d/filebot.sources" "https://raw.githubusercontent.com/filebot/plugins/master/apt/filebot.sources"
 
-# 2. Add deb repository to sources.list
-echo "deb [arch=all signed-by=/usr/share/keyrings/filebot.gpg] https://get.filebot.net/deb/ universal main" | sudo tee "/etc/apt/sources.list.d/filebot.list"
-
-# 3. Update package index
+# 2. Update package index
 sudo apt-get update
 
-# 4. Install dependencies explicitly (otherwise apt-get autoremove may purge them)
+# 3. Install dependencies explicitly (otherwise apt-get autoremove may purge them)
 sudo apt-get install -y --install-recommends default-jre openjfx zenity mediainfo libchromaprint-tools p7zip-full unrar
 
-# 5. Install FileBot
+# 4. Install FileBot
 sudo apt-get install -y --install-recommends filebot
 
-# 6. Test Run
+# 5. Test Run
 filebot -script fn:sysinfo
